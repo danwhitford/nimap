1069e1b70c75a656f136b778534c8f6d5f159ab5
Daniel Whitford <whitford.dsw@gmail.com>
2020-10-11 16:18:51 +0100

    raw socket impl

--- scan.nim
+++ scan.nim
@@ -1,6 +1,6 @@
 import net
 import parseopt
-from strutils import parseInt
+from strutils import parseInt, split, toHex
 
 
 var foundOpen: seq[string]
@@ -37,11 +37,53 @@ if startPort == -1 or endPort == -1:
     echo "Must set startPort and endPort"
     quit(QuitFailure)
 
+proc ipToHexString(ip: string): string =
+    var ip = ip
+    if ip == "localhost":
+        ip = "127.0.0.1"
+    assert ip.isIpAddress()
+    let parts = ip.split(".")
+    for part in parts:
+        result.add(chr(parseInt(part)))
+
+proc portToHexString(port: int): string =
+    let a = (port shr 8) and 0xff
+    let b = port and 0xff
+    return chr(a) & chr(b)
+
+proc generateIpHeader(sourceAddress: string, destinationAddress: string): string =
+    var ipHeader: string
+    ipHeader.add("\x45\x00\x00\x28")  # Version, IHL, Type of Service | Total Length
+    ipHeader.add("\xab\xcd\x00\x00")  # Identification | Flags, Fragment Offset
+    ipHeader.add("\x40\x06\xa6\xec")  # TTL, Protocol | Header Checksum
+    ipHeader.add(ipToHexString(sourceAddress))  # Source Address
+    ipHeader.add(ipToHexString(destinationAddress))  # Destination Addressor
+    return ipHeader
+
+proc generateTcpHeader(sourcePort: int, destinationPort: int): string =
+    var tcpHeader: string
+    tcpHeader.add(portToHexString(sourcePort) & portToHexString(destinationPort)) # Source Port | Destination Port
+    tcpHeader.add("\x00\x00\x00\x00") # Sequence Number
+    tcpHeader.add("\x00\x00\x00\x00") # Acknowledgement Number
+    tcpHeader.add("\x50\x02\x71\x10") # Data Offset, Reserved, Flags | Window Size
+    tcpHeader.add("\xe6\x32\x00\x00") # Checksum | Urgent Pointer)
+    return tcpHeader
+
+assert "\x7f\x00\x00\x01" == ipToHexString("localhost")
+assert "\x13\x8d" == portToHexString(5005)
+
 echo "Scanning ", host, " from ", startPort, " to ", endPort
 for port in startPort..endPort:
-    let socket = newSocket()
+    let socket = newSocket(AF_INET, SOCK_RAW, IPPROTO_RAW)
+    # socket.setSockOpt()
     try:
         socket.connect(host, Port(port))
+        let (laddr, lport) = socket.getLocalAddr()
+        let ipHeader = generateIpHeader("localhost", host)
+        let tcpHeader = generateTcpHeader(int(lport), port)
+        socket.send(ipHeader & tcpHeader)
+        let data = socket.recv(1024, timeout=5000)
+        echo data
         foundOpen.add($port)
     except:
         let m = getCurrentExceptionMsg()
